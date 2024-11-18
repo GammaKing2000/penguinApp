2d3fe601bad3bfd76a195acb5acd0940a8485fb3
Imaim <i.maimxz@gmail.com>
2024-11-18 13:40:25 +1100

    Select Calendar

--- penguinApp/ContentView.swift
+++ penguinApp/ContentView.swift
@@ -1,21 +1,24 @@
 //
 //  ContentView.swift
-//  penguinApp
+//  Penguin1
 //
-//  Created by Adit Vikram Mishra on 18/11/2024.
+//  Created by Nutnaree Duangdee on 18/11/2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("hey, world!")
+        VStack (alignment: .leading) {
+            Text("Choose your calendars")
+                .font(.title)
+                .fontWeight(.bold)
+            Text("Please choose your calendar so we can help manage your schedule and  provide suggestions.")
+                .font(.body)
+            
         }
-        .padding()
+        .frame(width: 330)
+//        .padding(EdgeInsets(top: -300, leading: -20, bottom: 0, trailing: 0))
     }
 }
 
