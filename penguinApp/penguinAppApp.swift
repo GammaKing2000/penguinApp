890fec0cabb717df4032f1bf93b414c27056dc48
adit <aditvikram15@gmail.com>
2024-11-18 13:02:08 +1100

    Initial Commit

---
+++ penguinApp/penguinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  penguinAppApp.swift
+//  penguinApp
+//
+//  Created by Adit Vikram Mishra on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct penguinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
