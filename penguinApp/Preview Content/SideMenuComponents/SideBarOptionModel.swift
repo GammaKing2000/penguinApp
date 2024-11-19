5d0f00493a2ee8bec48309c389ee4d6ce796bc61
adit <aditvikram15@gmail.com>
2024-11-19 18:26:54 +1100

    added side bar in AiChatView

---
+++ penguinApp/Preview Content/SideMenuComponents/SideBarOptionModel.swift
@@ -0,0 +1,8 @@
+//
+//  SideBarOptionModel.swift
+//  penguinApp
+//
+//  Created by Adit Vikram Mishra on 19/11/2024.
+//
+
+import Foundation
---
+++ penguinApp/Preview Content/SideMenuComponents/SideBarRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SideBarRowView.swift
+//  penguinApp
+//
+//  Created by Adit Vikram Mishra on 19/11/2024.
+//
+
+import SwiftUI
+
+struct SideBarRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideBarRowView()
+}
